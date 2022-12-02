7e4c7f823019d623fdc12501140d2cb01616c02c
Adeoniye Adelu <adeluadeoniye14@gmail.com>
2022-12-02 18:27:29 +0100

    BaseURL changed

--- src/axios/Base.js
+++ src/axios/Base.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
-export const url = 'https://it8-academy.herokuapp.com/api/v1';
+export const url = 'https://it8.onrender.com/api/v1';
 
 export const baseUrl = axios.create({
     baseURL: `${url}/auth`,
   });
+
+  
