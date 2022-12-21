2397f7100ada7cd61fbb02d256297bf68b5ce5d6
Adeoniye Adelu <adeluadeoniye14@gmail.com>
2022-12-21 17:59:34 +0100

    Source added to package.json

--- src/axios/Base.js
+++ src/axios/Base.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const url = 'https://it8.onrender.com/api/v1';
+export const url = 'https://api.it8-academy.com/api/v1';
 
 export const baseUrl = axios.create({
     baseURL: `${url}/auth`,
